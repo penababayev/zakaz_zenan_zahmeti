6f5daca8e242d54db70d144923142fc1955b254d
Pena <p.babayef@gmail.com>
2025-11-28 10:21:50 -0800

    updated

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key")
-DEBUG = os.getenv("DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "False"
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "34.61.30.58"]  # son uytgetmeli bolup biler
 
 
