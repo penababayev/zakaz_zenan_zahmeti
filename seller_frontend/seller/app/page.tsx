41b8e1da71fdb4842479a7f408e876907ede8bb7
Vepa Babayev <vepababayev03@gmail.com>
2025-12-27 00:53:30 +0100

    seeler frontend edildi

---
+++ seller_frontend/seller/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, doloremque? Nulla, labore molestiae nostrum iste quisquam dolore reiciendis suscipit velit excepturi iusto voluptatem fugit quas enim necessitatibus eos corrupti quae!</p>
+    </div>
+  )
+}
+
+export default Home
