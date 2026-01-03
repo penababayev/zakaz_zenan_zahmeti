c7ada90f7c54ae9f4d80dfbda4663ead36db945e
Ubuntu <raabazaar@gmail.com>
2026-01-04 02:01:12 +0500

    added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Zenan Zahmeti",
@@ -21,18 +10,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="font-poppins">
       <body className="font-poppins antialiased">
         <div className="flex flex-col min-h-screen">
-          <Header/>
+          <Header />
           <main className="flex-1">{children}</main>
-          <Footer/>
+          <Footer />
         </div>
-        
       </body>
     </html>
   );
